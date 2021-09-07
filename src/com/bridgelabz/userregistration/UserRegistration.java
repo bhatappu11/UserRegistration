e9b5199037870dcc4601de594d02007a1847d35a
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-07 22:08:10 +0530

    ADDED: method to check for valid last name

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -5,18 +5,39 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+	static Pattern pattern;
+	static Matcher matcher;
 	public static void main(String[] args) {
 		checkFirstName();
+		checkLastName();
+	}
+
+	private static void checkLastName() {
+		Scanner scanner=new Scanner(System.in);
+		pattern = Pattern.compile("^[A-Z][a-z]{2,}$");
+		while(true) {
+			System.out.println("Enter the last name");
+			String lastName = scanner.next();
+			matcher = pattern.matcher(lastName);
+			if(matcher.matches()) {
+				System.out.println("Valid"); 
+				break;
+			}
+			else {
+				System.out.println("Invalid"); 
+				continue;
+			}
+		}
+		scanner.close();
 	}
 
 	private static void checkFirstName() {
 		Scanner scanner=new Scanner(System.in);
-		String regex = "^[A-Z][a-z]{2,}$";
-		Pattern pattern = Pattern.compile(regex);
+		pattern = Pattern.compile("^[A-Z][a-z]{2,}$");
 		while(true) {
 			System.out.println("Enter the first name");
 			String firstName = scanner.nextLine();
-			Matcher matcher = pattern.matcher(firstName);
+			matcher = pattern.matcher(firstName);
 			if(matcher.matches()) {
 				System.out.println("Valid"); 
 				break;
