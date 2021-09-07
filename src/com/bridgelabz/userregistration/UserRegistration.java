632b28ae613ad960e14042ecdfd82ab33892aa7b
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-07 21:59:28 +0530

    ADDED: method to check for valid first name

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -1,10 +1,32 @@
 package com.bridgelabz.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		checkFirstName();
+	}
 
+	private static void checkFirstName() {
+		Scanner scanner=new Scanner(System.in);
+		String regex = "^[A-Z][a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		while(true) {
+			System.out.println("Enter the first name");
+			String firstName = scanner.nextLine();
+			Matcher matcher = pattern.matcher(firstName);
+			if(matcher.matches()) {
+				System.out.println("Valid"); 
+				break;
+			}
+			else {
+				System.out.println("Invalid"); 
+				continue;
+			}
+		}
+		scanner.close();
 	}
 
 }
